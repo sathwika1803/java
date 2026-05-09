c5ae07ae6d313eb6790762aae533219499793337
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-09 21:26:40 +0530

    swap

---
+++ swap.java
@@ -0,0 +1,11 @@
+public class swap {
+    static int a=10;
+    static int b=20;
+    public static void main(String[] args) {
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
