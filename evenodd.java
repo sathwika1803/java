fee610ac0d48995d97c28ccb846afe8314d162e9
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-09 21:28:26 +0530

    evenodd

---
+++ evenodd.java
@@ -0,0 +1,11 @@
+public class evenodd {
+    public static void main(String[] args) {
+        int a=20;
+        if (a%2==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd Number");
+        }
+    }
+}
