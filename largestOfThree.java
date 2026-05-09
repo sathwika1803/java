00ae1633e65625dfbb89aca7aa2f4a8e55f3186e
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-09 21:32:56 +0530

    largenumber

---
+++ largestOfThree.java
@@ -0,0 +1,19 @@
+public class largestOfThree {
+    public static void main(String[] args) {
+        int a=50;
+        int b=20;
+        int c=30;
+        if ((a>b) && (a>c)){
+            System.out.println(a);
+       
+
+        }
+         else if ((b>a)&&(b>c)){
+System.out.println(b);
+
+        }
+        else{
+            System.out.println(c);
+        }
+    }
+}
