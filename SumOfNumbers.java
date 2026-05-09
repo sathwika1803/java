dcd18de7b0eb877b60f1f51178d11f2824a3a8b9
sathwika1803 <rathkanthisathwika0305@gmail.com>
2026-05-09 21:44:17 +0530

    while

---
+++ SumOfNumbers.java
@@ -0,0 +1,12 @@
+public class SumOfNumbers {
+     static int sum=0;
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++){
+            
+            sum+=i;
+
+        }
+        System.out.println(sum);
+    }
+    
+}
